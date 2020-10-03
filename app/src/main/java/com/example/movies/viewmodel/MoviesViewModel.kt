6c0ed31f64bbaf0adcf29f91fa7aa0e629bc8e9b
brunohensel <bruno.hensel@gmail.com>
2020-10-03 11:41:57 +0200

    implemented a viewmodel to serve as a bridge between the repository and the view, passing the MovieState through a live data object.

--- app/src/main/java/com/example/movies/viewmodel/MoviesViewModel.kt
+++ app/src/main/java/com/example/movies/viewmodel/MoviesViewModel.kt
@@ -1,10 +1,31 @@
 package com.example.movies.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.movies.repository.MoviesRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
+@ExperimentalCoroutinesApi
 @Singleton
-class MoviesViewModel @Inject constructor(private val repository: MoviesRepository): ViewModel() {
+class MoviesViewModel @Inject constructor(private val repository: MoviesRepository) : ViewModel() {
+
+    private val _state = MutableLiveData<MovieState>()
+    val state: LiveData<MovieState> get() = _state
+
+    fun fetchMovies() =
+        viewModelScope.launch {
+            repository
+                .fetchMovies()
+                .onStart { MovieLoading }
+                .catch { error -> MovieFailure(error.stackTraceToString()) }
+                .collect { movies -> _state.value = MovieSuccess(movies) }
+        }
 }
