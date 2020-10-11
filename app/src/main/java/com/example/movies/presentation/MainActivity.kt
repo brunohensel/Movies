5c4032cad93bab63b392b3baf1e8bd41ff42652b
brunohensel <bruno.hensel@gmail.com>
2020-10-11 15:32:28 +0200

    handled menu clicks and removed the favorite fragment.

--- app/src/main/java/com/example/movies/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movies/presentation/MainActivity.kt
@@ -18,11 +18,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navHostFragment: NavHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
         val navController: NavController = navHostFragment.navController
+
         val appBarConfiguration = AppBarConfiguration(navController.graph)
+
         topAppBar.setupWithNavController(navController, appBarConfiguration)
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    findNavController(this, R.id.nav_host_fragment).navigate(R.id.searchFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onSupportNavigateUp() =
